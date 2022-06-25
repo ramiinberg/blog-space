959cc9cbc17e71dfcf6235447853408f8e33a130
ramiinberg <rami.inberg@gmail.com>
2022-06-25 10:32:28 +0300

    show fetched data in html

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
   .then(response => response.json())
   .then(data => {
-    const slicedArray = data.slice(0, 5)
-    console.log(slicedArray)
+    const postsArray = data.slice(0, 5)
+
+    const postHTML = postsArray.map(item => {
+      return `
+        <h2>${item.title}</h2>
+        <p>${item.body}</p>
+      `
+    }).join("")
+    document.getElementById("show-posts").innerHTML = postHTML
   })
